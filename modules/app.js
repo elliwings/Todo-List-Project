cd8ae41bf8fe6adfb866a41f7b8bd88116deb43d
elliwings <elizabethsokur4@gmail.com>
2024-07-12 14:13:49 +0300

    completed project

--- modules/app.js
+++ modules/app.js
@@ -1,7 +1,7 @@
+import './form.js';
+import './filter.js';
 import { loadTheme, loadData } from './storage.js';
 import { page } from './constants.js';
-import { formAddNewTask, submitForm } from './form.js';
-import { filterTasks } from './filter.js';
 
 (() => {
   loadData();
